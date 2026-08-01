b3bfd0991562f56ed1f66df951c12842911c1061
Truong-bell <ccc248591@gmail.com>
2026-08-01 14:35:50 +0700

    Update Combos.lua

--- Combos.lua
+++ Combos.lua
@@ -4,68 +4,19 @@
 -- ============================================================
 
 local combos = {
-    Saitama = {
-        "3M1-Uppercut-4M1-Beatdown",
-        "3M1-Downsmash-4M1-Consecutive",
-        "3M1-Uppercut-Downsmash-4M1"
-    },
-    Garou = {
-        "3M1-2-3M1-1-4",
-        "2-1-3M1-4",
-        "3M1-2-3M1-1-3M1-4"
-    },
-    Genos = {
-        "3M1-Downsmash-MachineGun-3M1-Ignition",
-        "3M1-JetDive-BlitzShot-4M1",
-        "3M1-MachineGun-3M1-Ignition-4M1"
-    },
-    Sonic = {
-        "3M1-QuickSlice-4M1-PinPoint",
-        "2-1-3M1-4",
-        "3M1-4M1-2-1"
-    },
-    ["Metal Bat"] = {
-        "3M1-GrandSlam-4M1-Homerun",
-        "3M1-Uppercut-GrandSlam-4M1",
-        "4M1-dash-FoulBall-4M1"
-    },
-    ["Atomic Samurai"] = {
-        "3M1-Downsmash-AtmosCleave",
-        "Shove-3M1-AtmosCleave",
-        "4M1-DashForward-AtmosCleave"
-    },
-    Tatsumaki = {
-        "M1x4-Windstorm-CrushingPull",
-        "3M1-SideDash-M1x4-Expulsive",
-        "3M1-side-Windstorm-4M1"
-    },
-    Suiryu = {
-        "3M1-Downsmash-BulletBarrage",
-        "3M1-BulletBarrage-Whirlwind",
-        "3M1-HeadFirst-Whirlwind-4M1"
-    },
-    ["Child Emperor"] = {
-        "3M1-Downsmash-WeBoom-ForwardDash",
-        "3M1-TrinityTear-DoubleTrouble",
-        "M1x4-WeBoom-TrinityTear"
-    },
-    Zombieman = {
-        "3M1-AxeSwing-DualPistols",
-        "M1x4-ShotgunBlast-AxeCombo",
-        "3M1-DualPistols-Reload"
-    },
-    Gojo = {
-        "3M1-Uptilt-Downsmash-Blue-3M1-Red",
-        "3M1-Blue-Downsmash-Red-4M1",
-        "3M1-Uptilt-Downsmash-Beatdown"
-    },
-    KJ = {
-        "4M1-Sprint-Kick-WallCombo",
-        "3M1-Downsmash-CriticalBlow",
-        "M1x4-SideDash-Jump-M1"
-    },
+    Saitama = {"3M1-Uppercut-4M1-Beatdown","3M1-Downsmash-4M1-Consecutive","3M1-Uppercut-Downsmash-4M1"},
+    Garou = {"3M1-2-3M1-1-4","2-1-3M1-4","3M1-2-3M1-1-3M1-4"},
+    Genos = {"3M1-Downsmash-MachineGun-3M1-Ignition","3M1-JetDive-BlitzShot-4M1","3M1-MachineGun-3M1-Ignition-4M1"},
+    Sonic = {"3M1-QuickSlice-4M1-PinPoint","2-1-3M1-4","3M1-4M1-2-1"},
+    ["Metal Bat"] = {"3M1-GrandSlam-4M1-Homerun","3M1-Uppercut-GrandSlam-4M1","4M1-dash-FoulBall-4M1"},
+    ["Atomic Samurai"] = {"3M1-Downsmash-AtmosCleave","Shove-3M1-AtmosCleave","4M1-DashForward-AtmosCleave"},
+    Tatsumaki = {"M1x4-Windstorm-CrushingPull","3M1-SideDash-M1x4-Expulsive","3M1-side-Windstorm-4M1"},
+    Suiryu = {"3M1-Downsmash-BulletBarrage","3M1-BulletBarrage-Whirlwind","3M1-HeadFirst-Whirlwind-4M1"},
+    ["Child Emperor"] = {"3M1-Downsmash-WeBoom-ForwardDash","3M1-TrinityTear-DoubleTrouble","M1x4-WeBoom-TrinityTear"},
+    Zombieman = {"3M1-AxeSwing-DualPistols","M1x4-ShotgunBlast-AxeCombo","3M1-DualPistols-Reload"},
+    Gojo = {"3M1-Uptilt-Downsmash-Blue-3M1-Red","3M1-Blue-Downsmash-Red-4M1","3M1-Uptilt-Downsmash-Beatdown"},
+    KJ = {"4M1-Sprint-Kick-WallCombo","3M1-Downsmash-CriticalBlow","M1x4-SideDash-Jump-M1"},
 }
-
 local characters = {"Saitama","Garou","Genos","Sonic","Metal Bat","Atomic Samurai","Tatsumaki","Suiryu","Child Emperor","Zombieman","Gojo","KJ"}
 
 return { combos = combos, characters = characters }
